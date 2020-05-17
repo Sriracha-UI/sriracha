c8d2de0e9facead94c26820eabf540c2e0cc18a9
JimmyMcBride <mcbride967@gmail.com>
2020-05-17 16:43:49 -0500

    btn edit

--- styled/maps/styles/button/radius.js
+++ styled/maps/styles/button/radius.js
@@ -3,7 +3,7 @@ import styledMap from "styled-map";
 export default styledMap`
   square: 0.1rem;
   rounded: 0.5rem;
-  smooth: 25%;
-  oval: 50%;
+  smooth: 1rem;
+  oval: 2rem;
   default: 0.3rem 0.8rem;
 `;
