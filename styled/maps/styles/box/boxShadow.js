c9c33d077730a967ec4463d3f450bbde5fe16121
JimmyMcBride <mcbride967@gmail.com>
2020-05-17 22:39:15 -0500

    default shadow edit

--- styled/maps/styles/box/boxShadow.js
+++ styled/maps/styles/box/boxShadow.js
@@ -2,7 +2,7 @@ import styledMap from "styled-map";
 import { theme } from "../../../theme";
 
 export default styledMap`
-  shade: ${theme.shadows[3]};
-  backlight: ${theme.glows[3]};
+  shade: ${theme.shadows[5]};
+  backlight: ${theme.glows[5]};
   default: none;
 `;
