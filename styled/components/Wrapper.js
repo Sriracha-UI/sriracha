ab01233d172323de568290414b3568c0cd0fdf03
JimmyMcBride <mcbride967@gmail.com>
2020-05-18 01:31:28 -0500

    hide it!

--- styled/components/Wrapper.js
+++ styled/components/Wrapper.js
@@ -4,6 +4,7 @@ import { flexColumnBase, wrapperSizeBase } from "../base";
 const Wrapper = styled.div`
   ${flexColumnBase}
   ${wrapperSizeBase}
+  overflow: ${({ overflow }) => (overflow ? overflow : "hidden")}
 `;
 
 export default Wrapper;
