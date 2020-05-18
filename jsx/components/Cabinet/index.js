b7ee5a5925c0b96d8d19c5a50733c775cedfa761
JimmyMcBride <mcbride967@gmail.com>
2020-05-18 02:18:21 -0500

    update

--- jsx/components/Cabinet/index.js
+++ jsx/components/Cabinet/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { Flex, theme } from "../../../index";
+import { Card, theme } from "../../../index";
 
 const Cabinet = ({ active, toggle, children, ...rest }) => (
   <CabinetWrapper>
