1101e707c0ec3c978355c9a744f3ef690e4653c5
JimmyMcBride <mcbride967@gmail.com>
2020-06-03 16:53:56 -0500

    center tables

--- styled/components/Card.js
+++ styled/components/Card.js
@@ -1,16 +1,11 @@
 import styled from "styled-components";
-import {
-  flexColumnBase,
-  cardSizeBase,
-  cardStyleBase,
-  centerFontBase,
-} from "../base";
+import { flexColumnBase, cardSizeBase, cardStyleBase } from "../base";
 
 const Card = styled.div`
   ${flexColumnBase}
   ${cardSizeBase}
   ${cardStyleBase}
-  ${centerFontBase}
+  text-align: center;
 `;
 
 export default Card;
