21db35a3b6e19a4cc90d8e2df6771df2e239ee8a
JimmyMcBride <mcbride967@gmail.com>
2020-09-17 16:33:35 -0500

    color mode theming

--- jsx/components/AppContainer/index.js
+++ jsx/components/AppContainer/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import AppWrapper from "../../../styled/components/AppWrapper";
-import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "./theme";
 import { GlobalStyles } from "../../../styled/components/GlobalStyles";
 import { useDarkMode } from "../../hooks/useDarkMode";
+import ThemeWrapper from "../ThemeProvider";
 
 export default function AppContainer({ children, ...rest }) {
   const [theme] = useDarkMode();
   const themeMode = theme === "light" ? lightTheme : darkTheme;
   return (
-    <ThemeProvider theme={themeMode}>
+    <ThemeWrapper theme={themeMode}>
       <GlobalStyles />
       <AppWrapper {...rest}>{children}</AppWrapper>
-    </ThemeProvider>
+    </ThemeWrapper>
   );
 }
--- jsx/hooks/useDarkMode.js
+++ jsx/hooks/useDarkMode.js
@@ -2,21 +2,24 @@ import { useState, useEffect } from "react";
 
 export const useDarkMode = () => {
   const [theme, setTheme] = useState("light");
+  const [toggle, setToggle] = useState(false);
   // localStorage.getItem("theme") ||
   const toggleTheme = () => {
     if (theme === "light") {
       localStorage.setItem("theme", "dark");
       setTheme("dark");
+      setToggle(!toggle);
     } else {
       localStorage.setItem("theme", "light");
       setTheme("light");
+      setToggle(!toggle);
     }
   };
 
   useEffect(() => {
-    const localTheme = localStorage.getItem("theme");
+    const localTheme = localStorage.getItem("theme") || "light";
     localTheme && setTheme(localTheme);
-  }, []);
+  }, [toggle]);
 
-  return [theme, toggleTheme];
+  return { theme, toggleTheme };
 };
