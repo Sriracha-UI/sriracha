ec9431b13d36c6a89bb7f20b683704003cafc7c5
JimmyMcBride <mcbride967@gmail.com>
2020-09-18 15:58:03 -0500

    please let this work fifth try

--- styled/base/strings/button/size.js
+++ styled/base/strings/button/size.js
@@ -3,99 +3,99 @@ import { sideways, btnMarg, btnPad } from "../../../maps";
 
 export default css`
   height: ${(props) =>
-    !props.bg
+    !props.h
       ? "auto"
-      : props.theme.space[props.bg]
-      ? props.theme.space[props.bg]
-      : props.bg};
+      : props.theme.space[props.h]
+      ? props.theme.space[props.h]
+      : props.h};
   width: ${(props) =>
-    !props.bg
+    !props.w
       ? sideways
-      : props.theme.space[props.bg]
-      ? props.theme.space[props.bg]
-      : props.bg};
+      : props.theme.space[props.w]
+      ? props.theme.space[props.w]
+      : props.w};
   max-height: ${(props) =>
-    !props.bg
+    !props.maxH
       ? "none"
-      : props.theme.space[props.bg]
-      ? props.theme.space[props.bg]
-      : props.bg};
+      : props.theme.space[props.maxH]
+      ? props.theme.space[props.maxH]
+      : props.maxH};
   max-width: ${(props) =>
-    !props.bg
+    !props.maxW
       ? "none"
-      : props.theme.space[props.bg]
-      ? props.theme.space[props.bg]
-      : props.bg};
+      : props.theme.space[props.maxW]
+      ? props.theme.space[props.maxW]
+      : props.maxW};
   min-height: ${(props) =>
-    !props.bg
+    !props.minH
       ? "none"
-      : props.theme.space[props.bg]
-      ? props.theme.space[props.bg]
-      : props.bg};
+      : props.theme.space[props.minH]
+      ? props.theme.space[props.minH]
+      : props.minH};
   min-width: ${(props) =>
-    !props.bg
+    !props.minW
       ? "none"
-      : props.theme.space[props.bg]
-      ? props.theme.space[props.bg]
-      : props.bg};
+      : props.theme.space[props.minW]
+      ? props.theme.space[props.minW]
+      : props.minW};
   margin: ${(props) =>
-    !props.bg
+    !props.m
       ? btnMarg
-      : props.theme.space[props.bg]
-      ? props.theme.space[props.bg]
-      : props.bg};
+      : props.theme.space[props.m]
+      ? props.theme.space[props.m]
+      : props.m};
   margin-top: ${(props) =>
-    !props.bg
+    !props.mt
       ? "none"
-      : props.theme.space[props.bg]
-      ? props.theme.space[props.bg]
-      : props.bg};
+      : props.theme.space[props.mt]
+      ? props.theme.space[props.mt]
+      : props.mt};
   margin-bottom: ${(props) =>
-    !props.bg
+    !props.mb
       ? "none"
-      : props.theme.space[props.bg]
-      ? props.theme.space[props.bg]
-      : props.bg};
+      : props.theme.space[props.mb]
+      ? props.theme.space[props.mb]
+      : props.mb};
   margin-left: ${(props) =>
-    !props.bg
+    !props.ml
       ? "none"
-      : props.theme.space[props.bg]
-      ? props.theme.space[props.bg]
-      : props.bg};
+      : props.theme.space[props.ml]
+      ? props.theme.space[props.ml]
+      : props.ml};
   margin-right: ${(props) =>
-    !props.bg
+    !props.mr
       ? "none"
-      : props.theme.space[props.bg]
-      ? props.theme.space[props.bg]
-      : props.bg};
+      : props.theme.space[props.mr]
+      ? props.theme.space[props.mr]
+      : props.mr};
   padding: ${(props) =>
-    !props.bg
+    !props.p
       ? btnPad
-      : props.theme.space[props.bg]
-      ? props.theme.space[props.bg]
-      : props.bg};
+      : props.theme.space[props.p]
+      ? props.theme.space[props.p]
+      : props.p};
   padding-top: ${(props) =>
-    !props.bg
+    !props.pt
       ? "none"
-      : props.theme.space[props.bg]
-      ? props.theme.space[props.bg]
-      : props.bg};
+      : props.theme.space[props.pt]
+      ? props.theme.space[props.pt]
+      : props.pt};
   padding-bottom: ${(props) =>
-    !props.bg
+    !props.pb
       ? "none"
-      : props.theme.space[props.bg]
-      ? props.theme.space[props.bg]
-      : props.bg};
+      : props.theme.space[props.pb]
+      ? props.theme.space[props.pb]
+      : props.pb};
   padding-left: ${(props) =>
-    !props.bg
+    !props.pl
       ? "none"
-      : props.theme.space[props.bg]
-      ? props.theme.space[props.bg]
-      : props.bg};
+      : props.theme.space[props.pl]
+      ? props.theme.space[props.pl]
+      : props.pl};
   padding-right: ${(props) =>
-    !props.bg
+    !props.pr
       ? "none"
-      : props.theme.space[props.bg]
-      ? props.theme.space[props.bg]
-      : props.bg};
+      : props.theme.space[props.pr]
+      ? props.theme.space[props.pr]
+      : props.pr};
 `;
--- styled/base/strings/unique/toolTip.js
+++ styled/base/strings/unique/toolTip.js
@@ -1,5 +1,4 @@
 import { css } from "styled-components";
-import { getTooltipColor, getTooltipBackground } from "../../../functions";
 
 export default css`
   & .tooltip {
@@ -9,12 +8,19 @@ export default css`
     padding: ${(props) => (props.ttP ? props.ttP : "2rem")};
     width: ${(props) => (props.ttW ? props.ttW : "auto")};
     height: ${(props) => (props.ttH ? props.ttH : "auto")};
-    ${(props) => getTooltipColor(props, props.theme.colors.gray9)}
-    ${(props) =>
-      getTooltipBackground(props, props.theme.colors.gray0)}
-    top: ${(
-      props
-    ) => (props.ttTop ? props.ttTop : "none")};
+    color: ${(props) =>
+      !props.ttColor
+        ? props.theme.colors.gray9
+        : props.theme.colors[props.ttColor]
+        ? props.theme.colors[props.ttColor]
+        : props.ttColor};
+    background: ${(props) =>
+      !props.ttBg
+        ? props.theme.colors.gray0
+        : props.theme.colors[props.ttBg]
+        ? props.theme.colors[props.ttBg]
+        : props.ttBg};
+    top: ${(props) => (props.ttTop ? props.ttTop : "none")};
     right: ${(props) => (props.ttRight ? props.ttRight : "none")};
     bottom: ${(props) => (props.ttBottom ? props.ttBottom : "none")};
     left: ${(props) => (props.ttLeft ? props.ttLeft : "none")};
