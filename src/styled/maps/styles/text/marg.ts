73ea2a7b5b4b412bf99e124bbaff4388e05b4679
JimmyMcBride <mcbride967@gmail.com>
2020-10-15 01:46:22 -0500

    ts conversion, please typescript and webpack gods, let this work

--- src/styled/maps/styles/text/marg.ts
+++ src/styled/maps/styles/text/marg.ts
@@ -1,11 +1,11 @@
 import styledMap from "styled-map";
 
 export default styledMap`
-  xsm: 1rem;
-  sm: 2rem;
-  mm: 3rem;
-  lm: 4rem;
-  xlm: 5rem;
-  xxlm: 6rem;
-  default: 1rem;
+  xsm?: 1rem;
+  sm?: 2rem;
+  mm?: 3rem;
+  lm?: 4rem;
+  xlm?: 5rem;
+  xxlm?: 6rem;
+  default?: 1rem;
 `;
--- src/styled/maps/styles/text/pad.ts
+++ src/styled/maps/styles/text/pad.ts
@@ -1,11 +1,11 @@
 import styledMap from "styled-map";
 
 export default styledMap`
-  xsp: 1rem;
-  sp: 2rem;
-  mp: 3rem;
-  lp: 4rem;
-  xlp: 5rem;
-  xxlp: 6rem;
-  default: 0.8rem;
+  xsp?: 1rem;
+  sp?: 2rem;
+  mp?: 3rem;
+  lp?: 4rem;
+  xlp?: 5rem;
+  xxlp?: 6rem;
+  default?: 0.8rem;
 `;
