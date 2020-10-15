c88e59594929dbe8756ccfb33e37ed3ec666081f
JimmyMcBride <mcbride967@gmail.com>
2020-10-15 02:39:22 -0500

    ts conversion, please typescript and webpack gods, let this work

--- src/styled/maps/styles/flex/justifyCenter.ts
+++ src/styled/maps/styles/flex/justifyCenter.ts
@@ -1,12 +1,12 @@
 import styledMap from "styled-map";
 
 const justifyCenter = styledMap`
-  jce: flex-end;
-  jcs: flex-start;
-  jcb: space-between;
-  jca: space-around;
-  jcv: space-evenly;
-  default: center;
+  jce!: flex-end;
+  jcs!: flex-start;
+  jcb!: space-between;
+  jca!: space-around;
+  jcv!: space-evenly;
+  default!: center;
 `;
 
 export default justifyCenter;
