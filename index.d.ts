508dd4152e60072d71ac965eeb98f7e8d04eb676
JimmyMcBride <mcbride967@gmail.com>
2020-10-15 02:56:41 -0500

    ts conversion, please typescript and webpack gods, let this work

--- index.d.ts
+++ index.d.ts
@@ -5,6 +5,6 @@ import { theme } from "./src/styled/theme";
 declare module "styled-components" {
   type Theme = typeof theme;
   export interface DefaultTheme extends Theme {
-    pr: string;
+    pr: any;
   }
 }
