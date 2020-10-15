2b7ccb1388b0bc234c7882d2daec7148d00bf116
JimmyMcBride <mcbride967@gmail.com>
2020-10-15 13:05:13 -0500

    ts conversion, please typescript and webpack gods, let this work

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,7 @@ module.exports = {
     extensions: [".tsx", ".ts", ".js"],
   },
   output: {
-    filename: "bundle.js",
+    filename: "index.js",
     path: path.resolve(__dirname, "dist"),
     libraryTarget: "umd",
   },
